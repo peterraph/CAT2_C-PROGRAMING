706a52c24227ff8f5166fc63babd0a3cf38d0fc9
peterraph <peterraphmaina@gmail.com>
2024-11-08 13:57:02 +0300

    Update CAT2_Q2.c

--- CAT2_Q2.c
+++ CAT2_Q2.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    // Declare and initialize a 3D array to represent two 2D arrays
-    int scores[2][2][2] = {
-        {{65, 92}, {84, 72}},  // First set
-        {{35, 70}, {59, 67}}   // Second set
-    };
-
-    // Display the 3D array values
-    printf("Scores:\n");
-    for (int i = 0; i < 2; i++) {
-        printf("Set %d:\n", i + 1);
-        for (int j = 0; j < 2; j++) {
-            for (int k = 0; k < 2; k++) {
-                printf("%d ", scores[i][j][k]);
-            }
-            printf("\n");
+int j,k;
+    int scores[4][2] = { {65, 92}, {84, 72},  {{35, 70}, {59, 67}} ;
+    for ( j = 0; j < 4; j++) {
+        for (k = 0; k < 2; k++) {
+        
+                printf(" scores[%d][%d]=%d\n,j,k,scores[j],[k])];
         }
-        printf("\n");
     }
 
     return 0;
